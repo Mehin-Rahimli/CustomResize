911b7e75eac50e33a81ed0e46c90b7e54d9b5885
Mehin-Rahimli <mehincr-bp217@code.edu.az>
2024-10-11 02:03:43 -0700

    initial commit

---
+++ CustomResize/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace CustomResize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 14, 15, 16, 17 };
+            CustomResize(ref numbers, 18, 19, 20,43653276,576,4323287);
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+
+
+        }
+        public static void CustomResize(ref int[] arr, params int[] nums)
+        {
+            int[] newArr = new int[arr.Length +nums.Length];
+            for(int i=0;i<arr.Length; i++)
+            {
+                newArr[i] = arr[i];
+            }
+            for(int i=0;i<nums.Length;i++)
+            {
+                newArr[arr.Length + i] = nums[i];
+            }
+            arr = newArr;
+        }
+    }
+}
